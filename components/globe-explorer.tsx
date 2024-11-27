60d7df0de03ec871ddf6c3b90470059e403ad2e0
zhangrui <zhangrui@student.ethz.ch>
2024-11-27 22:06:39 +0100

    saving current changes

--- components/globe-explorer.tsx
+++ components/globe-explorer.tsx
@@ -17,7 +17,8 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { Button } from '@/components/ui/button';
-import { ChevronsUpDown } from 'lucide-react';
+import { Check, ChevronsUpDown, X } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 interface MapPosition {
   coordinates: [number, number],
@@ -30,6 +31,25 @@ interface CountryChangeEvent {
 }
 
 interface GlobeExplorerProps { onCountryChange?: (e: CountryChangeEvent) => void }
+
+const countries = [
+  {
+    value: "Switzerland",
+    label: "Switzerland",
+  },
+  {
+    value: "Germany",
+    label: "Germany",
+  },
+  {
+    value: "England",
+    label: "England",
+  },
+  {
+    value: "USA",
+    label: "USA",
+  }
+]
 
 const GlobeExplorer = ({ 
   onCountryChange 
@@ -44,25 +64,8 @@ const GlobeExplorer = ({
 
   const [report, setReport] = useState("");
   const [position, setPosition] = useState<MapPosition>({ coordinates: [0, 0], zoom: 1.3 });
-  /*
-  function handleZoom(event: React.WheelEvent<SVGSVGElement>) {
-
-    if (event.shiftKey) {
-
-      if (event.deltaX > 0) {
-        position.zoom /= 1.1;
-        setPosition({...position});
-      }
-      if (event.deltaX < 0) {
-        position.zoom *= 1.1;
-        setPosition({...position});
-      }
-    }
-
-    console.log(position);
-
-  }
-    */
+  const [open, setOpen] = React.useState(false)
+  const [selectedCountry, setSelectedCountry] = useState<string>("");
 
   return <>
     <section id='world-map'>
@@ -86,14 +89,15 @@ const GlobeExplorer = ({
           </Select>
           <Separator orientation='vertical' className='bg-slate-300 h-10'/>
           <section className='flex items-center gap-2'>
-            <Popover>
-              <PopoverTrigger asChild>
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild disabled={report == ""}>
                 <Button
                   variant="outline"
                   role="combobox"
                   className="w-[200px] justify-between"
                 >
-                  <span className='font-normal opacity-90'>Select a country...</span>
+                  {selectedCountry != "" ? countries.find((country) => country.value === selectedCountry)?.label
+                    : "Select Country..."}
                   <ChevronsUpDown className="opacity-50" />
                 </Button>
               </PopoverTrigger>
@@ -101,9 +105,26 @@ const GlobeExplorer = ({
                 <Command>
                   <CommandInput placeholder="Search a country..." className="h-9" />
                   <CommandList>
-                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandEmpty>No country found.</CommandEmpty>
                     <CommandGroup>
-                      { /* // todo */ }
+                      {countries.map((country) => (
+                        <CommandItem
+                          key={country.value}
+                          value={country.value}
+                          onSelect={(currentValue) => {
+                            setSelectedCountry(currentValue === selectedCountry ? "" : currentValue)
+                            setOpen(false)
+                          }}
+                        >
+                          {country.label}
+                          <Check
+                            className={cn(
+                              "ml-auto",
+                              selectedCountry === country.value ? "opacity-100" : "opacity-0"
+                            )}
+                          />
+                        </CommandItem>
+                      ))}
                     </CommandGroup>
                   </CommandList>
                 </Command>
@@ -119,41 +140,63 @@ const GlobeExplorer = ({
         </section>
       </header>
       <Separator className="w-screen h-px bg-slate-300"/>
-      <main className="w-full  aspect-[4/3] overflow-hidden bg-slate-200">
+      <main className="w-full  aspect-[4/3] overflow-hidden bg-slate-200 relative">
         {
           report == "" ? 
             <div className='w-full h-full flex items-center justify-center'>
               <p className="font-medium text-center">Select Report to View Map</p> 
             </div>
             : 
-            <ComposableMap>
-              <ZoomableGroup translateExtent={[[0, 65], [900, 535]]} minZoom={1.3} center={position.coordinates} zoom={position.zoom}>
-                <Geographies geography={geoUrl}>
-                  {({ geographies }) =>
-                    geographies.map(geo => (
-                      <Geography 
-                        
-                        fill='black'
-                        key={geo.rsmKey} 
-                        geography={geo} 
-                        onClick={(event) => {
+            <>
+              <ComposableMap onClick={(event) => {
+                setSelectedCountry("");
+              }}>
+                <ZoomableGroup translateExtent={[[0, 65], [900, 535]]} minZoom={1.3} center={position.coordinates} zoom={position.zoom}>
+                  <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                      geographies.map(geo => (
+                        <Geography 
+                          fill='black'
+                          key={geo.rsmKey} 
+                          geography={geo} 
+                          onClick={(event) => {
+                            event.stopPropagation();
+                            setSelectedCountry(geo.properties.name);
 
-                          event.preventDefault();
-                          console.log(report);
-
-                          if (onCountryChange) {
-                            onCountryChange({
-                              country: geo.properties.name,
-                              report: report
-                            });
-                          }
-                        }}
-                      />
-                    ))
-                  }
-                </Geographies> 
-              </ZoomableGroup>
-            </ComposableMap>
+                            if (onCountryChange) {
+                              onCountryChange({
+                                country: geo.properties.name,
+                                report: report
+                              });
+                            }
+                          }}
+                        />
+                      ))
+                    }
+                  </Geographies> 
+                </ZoomableGroup>
+              </ComposableMap>
+              <div className="absolute top-0">
+                <Popover open={selectedCountry != ""}>
+                  <PopoverTrigger></PopoverTrigger>
+                  <PopoverContent className='mx-5 w-[40vw] max-w-[400px]'>
+                    <header className='flex justify-between items-start'>
+                      <section>
+                        <h2>{selectedCountry}</h2>
+                        <p className='text-sm opacity-50'>in {report}</p>
+                      </section>
+                      {
+                        /*
+                        <button className='bg-transparent p-1'>
+                          <X color='black' size={20}/>
+                        </button>
+                        */
+                      }
+                    </header>
+                  </PopoverContent>
+                </Popover>
+              </div>
+          </>
         }
       </main>
     </section>
