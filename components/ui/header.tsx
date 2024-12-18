24e9f9b4cff22eabb951ebdaa3038da942e892d2
iwimmer <iwimmer@student.ethz.ch>
2024-12-18 01:21:36 +0100

    Responsive header implementation

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -22,9 +22,8 @@ import {
 } from "@/components/ui/select"
 
 import Link from "next/link";
-import { cn } from "@/lib/utils";
 import clsx from "clsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
@@ -33,121 +32,137 @@ import { useState } from "react";
 
 export const Header = ({}: {}) => {
   const [sidebar, setSidebar] = useState<boolean>(false)
+  
 
-  return (<>
-    <header 
-      className={clsx(
-        "sticky top-0 w-full h-full z-50",
-        "md:sticky md:bg-white md:h-auto md:overflow-x-auto md:no-scrollbar md:z-50"
+
+  return (
+    <header className={clsx(
+      "w-screen overflow-hidden",
+
+      "md:bg-background md:sticky md:top-0 md:left-0 md:z-40"
     )}>
       <div className={clsx(
-        "",
-        "md:flex md:flex-wrap md:flex-row md:w-auto md:items-center md:h-auto md:justify-between md:gap-x-5 md:gap-y-5 md:px-5 md:py-3 ",
+        "md:p-2 flex justify-between"
       )}>
         <section className={clsx(
-          "absolute top-0 z-50 w-screen h-auto flex gap-x-5 p-2",
-          "md:relative md:top-auto md:w-auto md:flex md:items-center md:gap-x-10 md:gap-y-5 md:flex-wrap",
-          {
-            "bg-transparent": sidebar,
-            "bg-white": !sidebar
-          }
+          "flex items-center gap-x-5",
+
+          "",
         )}>
-          <button className="md:hidden" onClick={() => (setSidebar(!sidebar))}>
-            <Menu/>
-          </button>
-          <Link href="/" className="flex items-center gap-4 cursor-pointer">
-            <Image
-              src="/icons/logo_onLight.svg"
-              alt="A globe with a smiling face and a sun."
-              width={30}
-              height={30}
-              className={clsx(
-                "dark:hidden transition-all duration-150 ease-in-out",
-                "md:translate-x-0",
-                {
-                  "translate-x-24": sidebar
-                }
-              )}
-            />
-            <Image
-              src="/icons/logo_onDark.svg"
-              alt="A globe with a sleepy face and a moon."
-              width={30}
-              height={30}
-              className={clsx(
-                "hidden dark:block transition-all duration-150 ease-in-out",
-                "md:translate-x-0",
-                {
-                  "translate-x-24": sidebar
-                }
-              )}
-            />
-            <h1 className={clsx(
-              "text-2xl font-medium text-nowrap",
-              "transition-all duration-150 ease-in-out translate-y-0",
-              "md:translate-y-0",
-              {
-                "-translate-y-10": sidebar
-              }
-            )}>Smiling Globe</h1>
-          </Link>
-        </section>
-        <section className={clsx(
-          "absolute pt-12 top-0 -translate-x-full transition-all duration-150 ease-in-out h-screen w-[196px] shadow-2xl bg-slate-100 z-40",
-          "md:relative md:pt-0 md:w-fit md:top-auto md:left-auto md:translate-x-0 md:flex md:items-center md:gap-x-10 md:gap-y-5 md:flex-wrap md:h-auto md:shadow-none md:bg-transparent",
-          {
-            "translate-x-0": sidebar,
-          }
-        )}>
-          
-          <NavigationMenu className={clsx(
-            "md:inline-flex",
-            {
-              "inline-flex": sidebar
-            }
+
+          <div className={clsx(
+            "fixed top-0 left-0 right-0 w-screen p-2 z-50 flex items-center gap-x-5",
+            sidebar ? "bg-transparent" : "bg-background",
+
+            "md:static md:w-fit md:p-0",
           )}>
-            <NavigationMenuList className={clsx(
-              "flex-col p-2 items-start",
-              "md:flex-row md:p-0"
-            )}>
-              <NavigationMenuItem className={cn({"ml-1": sidebar})}>
-                <NavigationMenuLink href="/#world-map" className={cn(navigationMenuTriggerStyle(), "p-2", {"bg-transparent": sidebar})}>
-                  World Map
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink href="/compare" className={cn(navigationMenuTriggerStyle(), "p-2", {"bg-transparent": sidebar})}>
-                  Compare
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink href="/leaderboard" className={cn(navigationMenuTriggerStyle(), "p-2", {"bg-transparent": sidebar})}>
-                  Leaderboard
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-          {/* div to be able to click beside the sidebar to close it */}
-          <div 
-            onClick={() => setSidebar(false)}
-            className={clsx(
-              "fixed top-0 block w-screen h-full",
+            <button onClick={() => (setSidebar(!sidebar))} className={clsx(
+              "pl-2 z-50",
+
               "md:hidden",
-              {
-                "left-[196px]": sidebar,
-                "hidden": !sidebar
-              }
-          )}></div>
+            )}>
+              <Menu/>
+            </button>
+            <Link href={"/"} className={clsx(
+              "flex gap-x-4 ",
+            )}>
+              <Image
+                src="/icons/logo_onLight.svg"
+                alt="A globe with a smiling face and a sun."
+                width={30}
+                height={30}
+                className={clsx(
+                  "dark:hidden transition-all duration-150 ease-in-out",
+                  sidebar ? "translate-x-[5.5rem]" : "translate-x-0",
+
+                  "md:translate-x-0",
+                )}
+              />
+              <Image
+                src="/icons/logo_onDark.svg"
+                alt="A globe with a sleepy face and a moon."
+                width={30}
+                height={30}
+                className={clsx(
+                  "hidden dark:block transition-all duration-150 ease-in-out",
+                  sidebar ? "translate-x-[5.5rem]" : "-translate-x-0",
+
+                  "md:translate-x-0",
+                )}
+              />
+              <h1 className={clsx(
+                "text-2xl font-medium text-nowrap",
+                "transition-transform duration-150 ease-in-out",
+                sidebar ? "-translate-y-12" : "-translate-y-0",
+              )}>
+                Smiling Globe
+              </h1>
+            </Link>
+          </div>
+
+          <div className={clsx(
+            "fixed top-0 left-0 pt-12 w-48 bg-background h-screen z-40 shadow-2xl",
+            "transition-transform duration-150 ease-in-out",
+            sidebar ? "translate-x-0" : "-translate-x-full",
+
+            "md:static md:translate-x-0 md:w-fit md:pt-0 md:bg-transparent md:h-fit md:shadow-none",
+          )}>
+            <NavigationMenu className={clsx(
+              "w-full [&>div]:w-full",
+
+              "md:w-fit md:[&>div]:w-fit"
+            )}>
+              <NavigationMenuList className={clsx(
+                "flex-col p-2 items-start w-full",
+
+                "md:flex-row md:p-0 md:w-fit"
+              )}>
+                <NavigationMenuItem className={clsx(
+                  "border-t-[1px] border-slate-300 dark:border-slate-800 w-full [&>a]:w-full [&>a]:justify-start",
+
+                  "md:border-0 md:w-fit md:[&>a]:w-fit"
+                )}>
+                  <NavigationMenuLink href="/#world-map" className={clsx(navigationMenuTriggerStyle(), "p-2", {"bg-transparent": sidebar})}>
+                    World Map
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className={clsx(
+                  "border-t-[1px] border-slate-300 dark:border-slate-800 w-full [&>a]:w-full [&>a]:justify-start",
+
+                  "md:border-0 md:w-fit md:[&>a]:w-fit"
+                )}>
+                  <NavigationMenuLink href="/compare" className={clsx(navigationMenuTriggerStyle(), "p-2", {"bg-transparent": sidebar})}>
+                    Compare
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem className={clsx(
+                  "border-y-[1px] border-slate-300 dark:border-slate-800 w-full [&>a]:w-full [&>a]:justify-start",
+
+                  "md:border-0 md:w-fit md:[&>a]:w-fit"
+                )}>
+                  <NavigationMenuLink href="/leaderboard" className={clsx(navigationMenuTriggerStyle(), "p-2", {"bg-transparent": sidebar})}>
+                    Leaderboard
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+          <div onClick={() => setSidebar(false)} className={clsx(
+            "fixed top-0 left-48 w-[calc(100%-12rem)] h-screen z-50 max-w-full",
+            sidebar ? "block" : "hidden",
+
+            "md:hidden md:static",
+          )}/>
         </section>
+        
         <section className={clsx(
-          "absolute bottom-0 -translate-x-48 z-50 transition-all duration-150 ease-in-out m-2",
-          "md:relative md:bottom-auto md:left-auto md:translate-x-0 md:flex md:items-center md:gap-4 md:visible md:m-0",
-          {
-            "translate-x-0": sidebar,
-          }
+          "fixed bottom-2 left-2 z-[51] translate-x-0 transition-transform duration-150 ease-in-out",
+          sidebar ? "translate-x-0" : "-translate-x-48",
+
+          "md:static md:translate-x-0"
         )}>
           <Select defaultValue="en">
-            <SelectTrigger className="w-[180px]">
+            <SelectTrigger className="w-[11rem]">
               <SelectValue placeholder="English"/>
             </SelectTrigger>
             <SelectContent>
@@ -159,12 +174,11 @@ export const Header = ({}: {}) => {
         </section>
       </div>
       <Separator className={clsx(
-        "w-screen h-px bg-slate-300 absolute top-12 z-50 translate-x-0 transition-transform duration-250 ease-in-out delay-100 visible",
-        "md:visible md:relative md:top-auto md:bottom-0 md:translate-x-0",
-        {
-          "translate-x-full invisible": sidebar
-        }
+        "w-screen h-px bg-slate-300 dark:bg-slate-800 fixed top-12 z-50 transition-transform duration-250 ease-in-out delay-100",
+        sidebar ? "translate-x-full invisible" : "translate-x-0 visible",
+
+        "md:static"
       )}/>
     </header>
-  </>)
+  )
 } 
